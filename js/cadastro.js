3940a3afc9ca940c96ce992b2f0359b36df20f50
Arthur-Moreno <128709066+ArthurMorenoDev@users.noreply.github.com>
2024-07-10 14:28:55 -0300

    Add files via upload

---
+++ js/cadastro.js
@@ -0,0 +1,21 @@
+function voltarInicio() {
+    window.location.replace('index.html');
+  }
+
+function cadastrarUsuario(username,email, password) {
+    // Salvando as informações no localStorage
+    localStorage.setItem('username', username);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    if (email === '' || password === '') {
+      alert('Preencha todos os campos.');
+      return;
+    }
+    else{
+      alert('cadastro feito com sucesso');
+    }
+  }
+
+
+  document.querySelector('button').addEventListener('click', voltarInicio);
